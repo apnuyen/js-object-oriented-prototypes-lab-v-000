23a789a418ce765a7bebdbb49fed7aef5098db94
An Nuyen <annuyen@gmail.com>
2018-01-31 15:27:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ BoardMember.prototype.veto = function(){
 BoardMember.prototype.approve = function(){
      return 'You can do that!'
    }
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
-//
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
-// }
+
+BoardMember.prototype.doCharity = function(){
+     return "I like to help people."
+   }
+
+BoardMember.prototype.releasePressStatement = function(){
+     return "You will see great things from Scuber."
+   }
+
+BoardMember.prototype.sayHi = function(){
+     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+   }
+ }
