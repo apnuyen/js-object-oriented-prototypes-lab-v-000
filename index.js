cb178cd277e40ebf21585338537537ad49dbea5d
An Nuyen <annuyen@gmail.com>
2018-01-31 15:26:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
-//   this.training = training
-//
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
-//   this.approve = function(){
-//     return 'You can do that!'
-//   }
+function BoardMember(name, homeState, training){
+   this.name = name
+   this.homeState = homeState
+   this.training = training
+}
+
+BoardMember.prototype.veto = function(){
+     return 'No, I must disagree'
+   }
+BoardMember.prototype.approve = function(){
+     return 'You can do that!'
+   }
 //   this.doCharity = function(){
 //     return "I like to help people."
 //   }
